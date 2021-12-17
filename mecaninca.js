9df1e36f21648eb024e2ff5d29ab08bc8ca1310b
BRENO@Breno <brenobrito_13@hotmail.com>
2021-12-17 19:06:46 -0300

    adicionando home page

--- mecaninca.js
+++ mecaninca.js
@@ -1,3 +1,4 @@
+
 window.onload =function(){
 
     var stage = document.getElementById('stage');
@@ -16,12 +17,13 @@ window.onload =function(){
     var qp = 20;//quantidade de peças do tabuleiro
     var ax=ay=15;//posicao inicial da cobra
 
+    
     var trail= [];//rastro da cobra
     tail= 5;
     
     function game(){
         px += vx;
-        py = vy;
+        py += vy;
         
         if (px<0){
             px = qp -1;
@@ -50,42 +52,43 @@ window.onload =function(){
                 tail =5;
             }
         }
+    
+
+        trail.push({x:px, y:py})
+        while (trail.length > tail) {
+            trail.shift();
+        }
+
+        if (ax==px && ay==py){
+            tail++;
+            ax = Math.floor(Math.random()*qp);
+            ay = Math.floor(Math.random()*qp);
+        }
+
     }
 
-    trail.push({x:px, y:py})
-    while (trail.length > tail) {
-        trail.shift();
-    }
 
-    if (ax==px && ay==py){
-        tail++;
-        ax = Math.floor(Math.random()*qp);
-        ay = Math.floor(Math.random()*qp);
-    }
-
-}
-
-
-function keyPush(event){
-    switch (event.keyCode) {
-        case 37: // Left
-            vx = -vel;
-            vy = 0;
-            break;
-        case 38: // up
-            vx = 0;
-            vy = -vel;
-            break;
-        case 39: // right
-            vx = vel;
-            vy = 0;
-            break;
-        case 40: // down
-            vx = 0;
-            vy = vel;
-            break;          
-        default:
-            
-            break;
+    function keyPush(event){
+        switch (event.keyCode) {
+            case 37: // Left
+                vx =- vel;
+                vy = 0;
+                break;
+            case 38: // up
+                vx = 0;
+                vy =- vel;
+                break;
+            case 39: // right
+                vx = vel;
+                vy = 0;
+                break;
+            case 40: // down
+                vx = 0;
+                vy = vel;
+                break;          
+            default:
+                
+                break;
+        }
     }
 }
